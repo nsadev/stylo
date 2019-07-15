c6892916f175144aeb368f5437e4ae3def3687f9
nsadev <47278974+nsadev@users.noreply.github.com>
2019-07-15 12:28:50 +0200

    Hompage created and broken into smaller components. Styling of menu item component finished.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomepPage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomepPage />
+    </div>
+  );
+}
+
+export default App;
