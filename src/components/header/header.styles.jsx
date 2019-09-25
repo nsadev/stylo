2fa9a07095581df36cb7b1f0581f3d5c175cddae
nsadev <47278974+nsadev@users.noreply.github.com>
2019-09-25 15:48:01 +0200

    Started move to styled-components

---
+++ src/components/header/header.styles.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const HeaderContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 150px;
+  padding: 10px 25px 25px 0;
+  font-weight: bold;
+  font-size: 40px;
+  letter-spacing: 0.1em;
+`;
+
+export const OptionsContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+export const OptionLink = styled(Link)`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
