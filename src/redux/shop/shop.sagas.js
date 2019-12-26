5e516b81bad02ee3ee4e9f24ba4ec039287c4bb8
nsadev <47278974+nsadev@users.noreply.github.com>
2019-12-26 15:15:23 +0100

    Shop sagas continued

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,32 @@
-import { takeEvery } from 'redux-saga/effects';
-
-import ShopActionTypes from './shop.types';
+import { 
+  takeEvery, 
+  call, 
+  put 
+} from 'redux-saga/effects';
+import { 
+  firestore, 
+  convertCollectionsSnapshotToMap 
+} from "../../firebase/firebase.utils";
+import { 
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+ } from "./shop.actions";
+import { ShopActionTypes } from './shop.types';
 
 export function* fetchCollectionsAsync() {
   yield console.log('Working');
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap, 
+      snapshot
+    ); 
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
